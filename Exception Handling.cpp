d6748dfc195c9582e75f0b92fd8a81de55dca334
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-06-03 22:18:32 +0530

    Update and rename Exception Handling to Exception Handling.cpp

---
+++ Exception Handling.cpp
@@ -0,0 +1,26 @@
+/* try: represents a block of code that can throw an exception.
+
+catch: represents a block of code that is executed when a particular exception is thrown.
+
+throw: Used to throw an exception. Also used to list the exceptions that a function throws, but doesn’t handle itself.*/
+
+
+// Simple Code for Exception Handling
+#include <iostream>
+using namespace std;
+int main(){
+    int x = -1;
+    cout << "Before try \n";
+    try{
+        cout << "Inside try \n";
+        if (x < 0){
+            throw(x);
+            cout << "After throw (Never executed) \n";
+        }
+    }
+    catch(int x){
+        cout << "Exception Caught \n";
+    }
+    cout << "After catch (Will be executed) \n";
+    return 0;
+}
