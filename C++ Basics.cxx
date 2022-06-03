60b29f25f45a227bc9fb6dba9dae0155fc892652
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-06-03 10:49:44 +0530

    Update C++ Basics.cxx

--- C++ Basics.cxx
+++ C++ Basics.cxx
@@ -1,1 +1,18 @@
-
+// Basic inheritance 
+#include <bits/stdc++.h>
+using namespace std;
+class Parent{
+    public:
+    int age_p;
+};
+class Child : public Parent{
+    public: int age_c;
+};
+int main() {
+    Child obj;
+    obj.age_p = 50;
+    obj.age_c = 20;
+    cout << "Age of Child is : " << obj.age_c;
+    cout << "\nAge of Parent is : " << obj.age_p;
+    return 0;
+}
