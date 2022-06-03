6ca1a2a3d95af47009e144a0d88aa5bdf4b7979e
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-06-03 11:02:22 +0530

    Update C++ Basics.cxx

--- C++ Basics.cxx
+++ C++ Basics.cxx
@@ -16,3 +16,31 @@ int main() {
     cout << "\nAge of Parent is : " << obj.age_p;
     return 0;
 }
+
+
+// Understanding public, protected and private mode of inheritance
+#include <bits/stdc++.h>
+using namespace std;
+class A{
+    public:
+    int x;
+    protected:
+    int y;
+    private:
+    int z;
+};
+class B : public A{
+    //cout << "x is public"
+    //cout << "y is protected"
+    //cout << "z is inaccessible"
+};
+class C : protected A{
+    //cout << "x is protected"
+    //cout << "y is protected"
+    //cout << "z is inaccessible"
+};
+class D : private A{
+    //cout << "x is private"
+    //cout << "y is private"
+    //cout << "z is inaccessible"
+};
