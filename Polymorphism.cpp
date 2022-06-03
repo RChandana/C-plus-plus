7577b40f1ffca534f9e40534bafd369d371604a8
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-06-03 12:58:14 +0530

    Update Polymorphism.cpp

--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -1,1 +1,29 @@
+// Compile-time Polymorphism
+
+// Function Overloading 
+
+#include <iostream>
+using namespace std;
+class Main{
+    public:
+    void func(int x){
+        cout << "Integer value of x is : " << x << endl;
+    };
+    public:
+    void func(double x){
+        cout << "Double value of x is : " << x << endl;
+    };
+    public:
+    void func(int x, int y){
+        cout << "Integer value of x : " << x << "\nInteger value of y : " << y << endl;
+    };
+};
+    int main(){
+        Main obj;
+        
+        obj.func(10);
+        obj.func(10.50);
+        obj.func(20, 30);
+        return 0;
+    }
 
