ec3cefd9a6b475b5ff8d0b2cfd3a57b8aa71a1af
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-06-04 12:29:07 +0530

    Update Arrays.cpp

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,1 +1,15 @@
+// Taking user input and diplaying the array
 
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    cout << "Enter five numbers : \n";
+    for(int i = 0; i < 5; i++){
+        cin >> arr[i];
+    }
+    for(int i = 0; i < 5; i++){
+        cout << arr[i];
+    }
+    return 0;
+}
