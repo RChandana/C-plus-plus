563ebd4fe992b2c78fc08012fa8aae9c3b92e423
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-06-04 12:30:08 +0530

    Update Arrays.cpp

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,3 +1,17 @@
+// Accessing values of an array
+
+#include <iostream>
+using namespace std;
+int arr[5] = {10,20,30,40,50};
+int main ()
+{
+cout << arr[3];
+return 0;
+}
+
+
+
+
 // Taking user input and diplaying the array
 
 #include <iostream>
