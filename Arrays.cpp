26330de8cf3d405351499e5d8ddc96cebfbf0759
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-06-04 12:32:45 +0530

    Update Arrays.cpp

--- Arrays.cpp
+++ Arrays.cpp
@@ -22,8 +22,9 @@ int main(){
     for(int i = 0; i < 5; i++){
         cin >> arr[i];
     }
+    cout << "The array is : ";
     for(int i = 0; i < 5; i++){
-        cout << arr[i];
+        cout  << arr[i] ;
     }
     return 0;
 }
