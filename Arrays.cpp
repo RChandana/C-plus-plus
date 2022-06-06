77188ebcbb7ca32f4f19b124e77d540b16991e67
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-06-06 18:34:26 +0530

    Update Arrays.cpp

--- Arrays.cpp
+++ Arrays.cpp
@@ -84,3 +84,39 @@ int main(){
         }
     return 0;
 }
+
+
+
+
+
+// Sorting the elements in acsending order.
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n, x, y;
+    cout << "Enter the number of elements : ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the numbers : \n";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    cout << "The array is : ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] ;
+    }
+    cout << "\nThe array in ascending order is : ";
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            if (arr[j] > arr[j + 1]){
+                y = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = y;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+     cout << arr[i];
+    return 0;
+}
